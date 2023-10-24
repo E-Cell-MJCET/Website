77cb5d8765610e100fb8bc1a72d4cc7914153900
legendhimslef <77602276+Abdulbasith0792@users.noreply.github.com>
2023-10-24 12:37:19 +0530

    s,df

--- components/home/navbar.tsx
+++ components/home/navbar.tsx
@@ -41,10 +41,7 @@ const Navbar = () => {
           MJCET
         </div>
 
-<<<<<<< Updated upstream
-=======
         
->>>>>>> Stashed changes
         {/* <div className='text-[14px]  mt-2 lg:max-w-[400px] poiret font-semibold text-gray-300'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga fugit eligendi dolores neque dolor sed sunt quisquam tempora 
           nesciunt? Quas nihil totam alias quos, sunt....</div> */}
         <div className="grid gap-8 items-start justify-center mt-10 mb-14 lg:my-20">
