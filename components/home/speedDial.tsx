1485e1994098c862fcbef21414891c0c13dd7d01
Mohammed Abid Nafi <abid.nafi04@gmail.com>
2024-02-25 17:52:23 +0530

    fix: warnings

--- components/home/speedDial.tsx
+++ components/home/speedDial.tsx
@@ -96,9 +96,9 @@ const SpeedDial = () => {
         >
           <path
             stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             d="M9 1v16M1 9h16"
           />
         </svg>
