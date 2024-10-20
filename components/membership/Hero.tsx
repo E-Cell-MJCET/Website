2623cd4e6548004f5166cdfc066b541c8b6918d6
E-Cell MJCET <ecellmjcet@mjcollege.ac.in>
2024-10-20 22:48:41 +0530

    feat: hero section and faq section (#44)
    * done
    
    * esLint issues fixed
    
    * feat: add Faq section
    
    * feat-Faq & hero
    
    * fix: lint errors
    
    * fix: lint erros
    
    * fix: lint erros
    
    ---------
    
    Co-authored-by: Syed Abdul Muneeb <samuneeb786@gmail.com>
    Co-authored-by: Mohammed Abid Nafi <abid.nafi04@gmail.com>

---
+++ components/membership/Hero.tsx
@@ -0,0 +1,142 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import Link from "next/link";
+
+import { ShootingStars } from "../ui/shootingstars";
+import { StarsBackground } from "../ui/starsbackground";
+
+const Hero = () => {
+  //3
+  useGSAP(() => {
+    gsap.from("span", {
+      duration: 1,
+      delay: 1.1,
+      opacity: 0,
+      stagger: 0.08,
+    });
+  });
+  //2
+  useGSAP(() => {
+    gsap.from(".demo", {
+      y: 15,
+      duration: 0.3,
+      delay: 0.8,
+    });
+  });
+  //1
+  useGSAP(() => {
+    gsap.from(".demo1", {
+      y: 20,
+      duration: 0.3,
+      delay: 0.5,
+    });
+  });
+  //2
+  useGSAP(() => {
+    gsap.from(".btn", {
+      y: -60,
+      duration: 0.3,
+      delay: 0.8,
+    });
+  });
+
+  useGSAP(() => {
+    gsap.to(".one", {
+      y: -10,
+      duration: 0.6,
+      delay: 3.1,
+      opacity: 0,
+    });
+  });
+  //4
+  useGSAP(() => {
+    gsap.from(".two", {
+      y: 10,
+      duration: 0.6,
+      delay: 3.7,
+      opacity: 0,
+    });
+  });
+
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center overflow-hidden bg-black px-4">
+      <Image
+        src="/icon-384x384.png"
+        alt="Ecell Logo"
+        width={304}
+        height={304}
+        className="demo1 z-10 mb-4 h-40 w-40 bg-clip-text sm:h-48 sm:w-48 lg:h-64 lg:w-64"
+      />
+      <p className="demo font-silkscreen z-10 hidden bg-clip-text text-3xl font-extrabold text-white sm:block sm:text-5xl ">
+        Entrepreneurship Cell
+      </p>
+      <p className="demo font-silkscreen z-10 bg-clip-text text-3xl font-extrabold text-white sm:hidden ">
+        E-Cell MJCET
+      </p>
+      <h1
+        className="font-silkscreen z-10 mt-2 items-center justify-center gap-3
+    bg-clip-text text-center text-2xl font-extrabold text-[#F6E445] sm:mt-6 sm:flex sm:flex-col sm:text-3xl lg:flex lg:flex-row lg:text-4xl"
+      >
+        <div>
+          <span>I</span>
+          <span>d</span>
+          <span>e</span>
+          <span>a</span>
+          <span>t</span>
+          <span>e</span>
+          <span>.</span>
+        </div>
+        <div>
+          <span>I</span>
+          <span>n</span>
+          <span>n</span>
+          <span>o</span>
+          <span>v</span>
+          <span>a</span>
+          <span>t</span>
+          <span>e</span>
+          <span>.</span>
+        </div>
+        <div>
+          <span>I</span>
+          <span>n</span>
+          <span>c</span>
+          <span>u</span>
+          <span>b</span>
+          <span>a</span>
+          <span>t</span>
+          <span>e</span>
+          <span>.</span>
+        </div>
+      </h1>
+      <div className="mt-12 flex w-96 flex-col items-center justify-center gap-5 sm:flex-row">
+        <Link
+          href={"/"}
+          className="btn font-silkscreen relative z-10 mt-12 flex h-12 w-52 items-center justify-center rounded bg-blue-700 px-4 py-3 text-center text-sm text-white hover:cursor-pointer hover:bg-blue-600 sm:px-7 sm:py-4 sm:text-lg"
+        >
+          <div className="two absolute inset-0 flex items-center justify-center">
+            Join us now
+          </div>
+          <div className="one absolute inset-0 flex flex-nowrap items-center justify-center text-sm">
+            Become a Member
+          </div>
+        </Link>
+        <button
+          onClick={() =>
+            window.scrollTo({ top: window.innerHeight, behavior: "smooth" })
+          }
+          className="btn font-silkscreen relative z-10 flex h-12 w-52 items-center justify-center rounded border-2 border-[#F6E445] px-4 py-3 text-center text-sm hover:cursor-pointer sm:mt-4 sm:px-7 sm:py-4 sm:text-lg lg:mt-12"
+        >
+          <p>Know more</p>
+        </button>
+      </div>
+      <ShootingStars />
+      <StarsBackground />
+    </div>
+  );
+};
+//
+export default Hero;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,12 @@
 import type { Config } from "tailwindcss";
 
+const svgToDataUri = require("mini-svg-data-uri");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 const config = {
+  darkMode: ["class"],
   // darkMode: ["class"],
   content: [
     "./pages/**/*.{ts,tsx}",
@@ -23,83 +29,80 @@ const config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-
       colors: {
         "card-light": "#e4e4e7",
         "card-dark": "#27272a",
         "ecell-red": {
-          100: "#ff403c",
+          "100": "#ff403c",
         },
         "ecell-blue": {
-          100: "#0b5db0",
+          "100": "#0b5db0",
         },
         gray: {
-          50: "#f9fafb",
-          100: "#f3f4f6",
-          200: "#e5e7eb",
-          300: "#d1d5db",
-          400: "#9ca3af",
-          500: "#6b7280",
-          600: "#4b5563",
-          700: "#374151",
-          800: "#1f2937",
-          900: "#111827",
-          950: "#030712",
+          "50": "#f9fafb",
+          "100": "#f3f4f6",
+          "200": "#e5e7eb",
+          "300": "#d1d5db",
+          "400": "#9ca3af",
+          "500": "#6b7280",
+          "600": "#4b5563",
+          "700": "#374151",
+          "800": "#1f2937",
+          "900": "#111827",
+          "950": "#030712",
         },
         slate: {
-          50: "#f8fafc",
-          100: "#f1f5f9",
-          200: "#e2eaf2",
-          300: "#cbd5e1",
-          400: "#94a3b8",
-          500: "#64748b",
-          600: "#475569",
-          700: "#334155",
-          800: "#1e293b",
-          900: "#0f172a",
-          950: "#020617",
+          "50": "#f8fafc",
+          "100": "#f1f5f9",
+          "200": "#e2eaf2",
+          "300": "#cbd5e1",
+          "400": "#94a3b8",
+          "500": "#64748b",
+          "600": "#475569",
+          "700": "#334155",
+          "800": "#1e293b",
+          "900": "#0f172a",
+          "950": "#020617",
         },
-
         zinc: {
-          50: "#fafafa",
-          100: "#f4f4f5",
-          200: "#e4e4e7",
-          300: "#d4d4d8",
-          400: "#a1a1aa",
-          500: "#717179",
-          600: "#52525a",
-          700: "#3f3f46",
-          800: "#27272a",
-          900: "#18181b",
-          950: "#09090b",
+          "50": "#fafafa",
+          "100": "#f4f4f5",
+          "200": "#e4e4e7",
+          "300": "#d4d4d8",
+          "400": "#a1a1aa",
+          "500": "#717179",
+          "600": "#52525a",
+          "700": "#3f3f46",
+          "800": "#27272a",
+          "900": "#18181b",
+          "950": "#09090b",
         },
         neutral: {
-          50: "#fafafa",
-          100: "#f5f5f5",
-          200: "#e5e5e5",
-          300: "#d4d4d4",
-          400: "#a3a3a3",
-          500: "#737373",
-          600: "#525252",
-          700: "#404040",
-          800: "#262626",
-          900: "#171717",
-          950: "#0a0a0a",
+          "50": "#fafafa",
+          "100": "#f5f5f5",
+          "200": "#e5e5e5",
+          "300": "#d4d4d4",
+          "400": "#a3a3a3",
+          "500": "#737373",
+          "600": "#525252",
+          "700": "#404040",
+          "800": "#262626",
+          "900": "#171717",
+          "950": "#0a0a0a",
         },
         stone: {
-          50: "#fafafb",
-          100: "#f5f5f4",
-          200: "#e7e5e4",
-          300: "#d6d3d1",
-          400: "#a8a29e",
-          500: "#78736f",
-          600: "#57534e",
-          700: "#44413c",
-          800: "#292521",
-          900: "#1c1917",
-          950: "#0c0a09",
+          "50": "#fafafb",
+          "100": "#f5f5f4",
+          "200": "#e7e5e4",
+          "300": "#d6d3d1",
+          "400": "#a8a29e",
+          "500": "#78736f",
+          "600": "#57534e",
+          "700": "#44413c",
+          "800": "#292521",
+          "900": "#1c1917",
+          "950": "#0c0a09",
         },
-
         black: "#000000",
         white: "#ffffff",
         transparent: "transparent",
@@ -116,7 +119,6 @@ const config = {
       backgroundSize: {
         "300%": "300%",
       },
-
       keyframes: {
         tilt: {
           "0%, 50%, 100%": {
@@ -129,31 +131,49 @@ const config = {
             transform: "rotate(-1.5deg)",
           },
         },
-
         "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
         },
         "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
         },
-
         animatedgradient: {
-          "0%": { backgroundPosition: "0% 50%" },
-          "50%": { backgroundPosition: "100% 50%" },
-          "100%": { backgroundPosition: "0% 50%" },
+          "0%": {
+            backgroundPosition: "0% 50%",
+          },
+          "50%": {
+            backgroundPosition: "100% 50%",
+          },
+          "100%": {
+            backgroundPosition: "0% 50%",
+          },
         },
-
         marquee: {
-          "0%": { transform: "translateX(0%)" },
-          "100%": { transform: "translateX(-100%)" },
+          "0%": {
+            transform: "translateX(0%)",
+          },
+          "100%": {
+            transform: "translateX(-100%)",
+          },
         },
         marquee2: {
-          "0%": { transform: "translateX(100%)" },
-          "100%": { transform: "translateX(0%)" },
+          "0%": {
+            transform: "translateX(100%)",
+          },
+          "100%": {
+            transform: "translateX(0%)",
+          },
         },
-
         fadein: {
           "0%": {
             opacity: "0",
@@ -163,14 +183,20 @@ const config = {
           },
         },
         screens: {
-          mobile: { max: "700px" },
-          desktop: { min: "640px" },
+          mobile: {
+            max: "700px",
+          },
+          desktop: {
+            min: "640px",
+          },
         },
       },
       fontFamily: {
         staatliches: ["var(--font-staatliches)"],
         montserrat: ["var(--font-montserrat)"],
         nanum: ["var(--font-nanum-pen-script)"],
+        silkscreen: ["var(--font-silkscreen)"],
+        pixelify_sans: ["var(--font-pixelify-`sans)"],
       },
       boxShadow: {
         ticket:
@@ -178,7 +204,33 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    addVariablesForColors,
+    function ({ matchUtilities, theme }: any) {
+      matchUtilities(
+        {
+          "bg-dot-thick": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="2.5"></circle></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
+  ],
 } satisfies Config;
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
 
 export default config;
