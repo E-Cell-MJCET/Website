6ce6585c9638b79eb6bd143171f194d773dc1168
Mohammed Abid Nafi <abid.nafi04@gmail.com>
2024-11-28 02:49:00 +0530

    fix: seo

--- app/profile/nazmeen/layout.tsx
+++ app/profile/nazmeen/layout.tsx
@@ -12,7 +12,7 @@ export const metadata = {
       "Discover the achievements and experiences of Nazmeen Sultana, a dedicated and innovative individual with a passion for leadership and collaboration.",
     url: "https://www.ecellmjcet.com/profile/nazmeen", // Change URL to match the actual profile URL
     type: "profile",
-    image: "/assets/GB/nazmeen.jpg", // Update with the actual image path
+    image: "https://www.ecellmjcet.com/assets/GB/nazmeen.jpg", // Update with the actual image path
   },
   twitter: {
     card: "summary_large_image",
@@ -22,14 +22,14 @@ export const metadata = {
       "Nazmeen Sultana, profile, leadership, collaboration, innovation, events, communication, connections, Ecell, ecell, ecellmjcet, Ecell Mjcet, E-Cell MJCET",
     description:
       "Explore the profile of Nazmeen Sultana, a creative problem-solver with a passion for leadership, collaboration, and innovation.",
-    image: "/assets/GB/nazmeen.jpg", // Update with the actual image path
+    image: "https://www.ecellmjcet.com/assets/GB/nazmeen.jpg", // Update with the actual image path
   },
   structuredData: {
     "@context": "https://schema.org",
     "@type": "Person",
     name: "Nazmeen Sultana",
     url: "https://www.ecellmjcet.com/profile/nazmeen",
-    image: "/assets/GB/nazmeen.jpg",
+    image: "https://www.ecellmjcet.com/assets/GB/nazmeen.jpg",
     jobTitle: "Execom",
     worksFor: {
       "@type": "Organization",
